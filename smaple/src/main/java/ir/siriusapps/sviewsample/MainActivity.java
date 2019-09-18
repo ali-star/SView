14d0c265f49bef47e848f769741d8cc7091205e2
ali-star <obanstar1@gmail.com>
2019-09-18 15:39:49 +0430

    update

--- smaple/src/main/java/ir/siriusapps/sviewsample/MainActivity.java
+++ smaple/src/main/java/ir/siriusapps/sviewsample/MainActivity.java
@@ -1,16 +1,8 @@
 package ir.siriusapps.sviewsample;
 
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Handler;
-import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
-import ir.siriusapps.sview.Utils;
-import ir.siriusapps.sview.widget.BlurBackgroundImageView;
-import ir.siriusapps.sview.widget.Button;
-import ir.siriusapps.sview.widget.ImageView;
-import ir.siriusapps.sview.widget.Loading;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,87 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        /*ImageView imageView = findViewById(R.id.imageView2);
-        SVG svg = SVGParser.getSVGFromResource(getResources(), R.raw.icon);
-        imageView.setImageDrawable(svg.createPictureDrawable());
-        */
-
-        /*final ArcLayout arcLayout = findViewById(R.id.shapeView);
-        ValueAnimator valueAnimator = ValueAnimator.ofFloat(0f, Utils.dipToPix(120));
-        valueAnimator.setDuration(800);
-        valueAnimator.setRepeatCount(ValueAnimator.INFINITE);
-        valueAnimator.setRepeatMode(ValueAnimator.REVERSE);
-        valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                arcLayout.setArcSize((Float) animation.getAnimatedValue());
-            }
-        });
-        valueAnimator.start();*/
-
-        final BlurBackgroundImageView imageView = findViewById(R.id.imageView3);
-        imageView.setImageBitmap(Utils.getCircularBitmapFrom(BitmapFactory.decodeResource(getResources(), R.drawable.app_profile)));
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int padding = Utils.dipToPix(86);
-                imageView.setPadding(padding);
-                imageView.setBlurRadius(15f, false);
-                imageView.setBlurAlpha(255, true);
-            }
-        });
-
-        final ImageView svgView = findViewById(R.id.imageView2);
-        svgView.setSvgResource(R.raw.menu);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                svgView.setSvgResource(R.raw.icon, Color.parseColor("#999999"));
-            }
-        }, 1500);
-
-        final Button button = findViewById(R.id.view);
-        findViewById(R.id.view).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                button.setCorerRadius(2);
-            }
-        });
-
-        final Loading loading = findViewById(R.id.loadingView);
-
-        final Handler handler = new Handler();
-
-        loading.setProgress(70);
-
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                loading.setLoading();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        loading.setUserRotationAnimationWithProgress(false);
-                        loading.setProgress((float) Math.floor(Math.random() * 100));
-                        handler.postDelayed(new Runnable() {
-                            @Override
-                            public void run() {
-                                loading.setUserRotationAnimationWithProgress(true);
-                                handler.postDelayed(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        loading.setUserRotationAnimationWithProgress(false);
-                                        loading.setProgress((float) Math.floor(Math.random() * 100));
-                                        handler.postDelayed(this, 3000);
-                                    }
-                                }, 3000);
-                            }
-                        }, 3000);
-                    }
-                }, 3000);
-            }
-        }, 6000);
 
 
     }
--- sview/src/main/java/ir/siriusapps/sview/widget/RelativeLayout.java
+++ sview/src/main/java/ir/siriusapps/sview/widget/RelativeLayout.java
@@ -4,36 +4,51 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Outline;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.RectF;
+import android.graphics.Typeface;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewOutlineProvider;
+
+import androidx.annotation.Nullable;
+
 import ir.siriusapps.sview.R;
 import ir.siriusapps.sview.SView;
+import ir.siriusapps.sview.TypefaceManager;
 import ir.siriusapps.sview.view.CornerView;
 
 public class RelativeLayout extends android.widget.RelativeLayout implements CornerView {
 
-    private Path clipPath = new Path();
+    private Path basePath = new Path();
     private Paint clipPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
     private PorterDuffXfermode porterDuffXfermode = new PorterDuffXfermode(PorterDuff.Mode.CLEAR);
     private RectF rectF = new RectF();
     private int cornerRadius;
+
+    private Paint basePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    private int shadowColor = Color.parseColor("#80000000");
+    private float shadowSize = 0;
+    private float shadowDy = 0;
 
     public RelativeLayout(Context context) {
         super(context);
         init(null);
     }
 
-    public RelativeLayout(Context context, AttributeSet attrs) {
+    public RelativeLayout(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         init(attrs);
     }
 
-    public RelativeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+    public RelativeLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init(attrs);
     }
@@ -43,40 +58,128 @@ public class RelativeLayout extends android.widget.RelativeLayout implements Cor
         if (attrs != null) {
             TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.SView);
             cornerRadius = typedArray.getDimensionPixelSize(R.styleable.SView_sview_cornerRadius, cornerRadius);
+            shadowColor = typedArray.getColor(R.styleable.SView_sview_shadowColor, shadowColor);
+            shadowSize = typedArray.getDimensionPixelSize(R.styleable.SView_sview_shadowSize, (int) shadowSize);
+            shadowDy = typedArray.getDimensionPixelSize(R.styleable.SView_sview_shadowDy, (int) shadowDy);
             typedArray.recycle();
         }
 
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            setLayerType(LAYER_TYPE_SOFTWARE, null);
-            setWillNotDraw(false);
-        }
+        setLayerType(LAYER_TYPE_SOFTWARE, null);
+        setWillNotDraw(false);
 
-        clipPath.setFillType(Path.FillType.INVERSE_WINDING);
         clipPaint.setXfermode(porterDuffXfermode);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+        if (getBackground() instanceof ColorDrawable) {
+            basePaint.setColor(((ColorDrawable) getBackground()).getColor());
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             setClipToOutline(true);
+            setOutlineProvider(new ViewOutlineProvider() {
+                @Override
+                public void getOutline(View view, Outline outline) {
+                    outline.setAlpha(0);
+                    int mCornerRadius = cornerRadius;
+                    if (mCornerRadius < 0)
+                        mCornerRadius = view.getHeight() / 2;
+                    outline.setRoundRect(0, 0, view.getWidth(), view.getHeight(), mCornerRadius);
+                }
+            });
+        }
+
+        if (shadowSize > 0) {
+            setBackground(null);
+            basePaint.setShadowLayer(shadowSize, 0, shadowDy, shadowColor);
+            setPadding((int) shadowSize + getPaddingLeft(),
+                    (int) (shadowSize - shadowDy) + getPaddingTop(),
+                    (int) shadowSize + getPaddingRight(),
+                    (int) (shadowSize + shadowDy) + getPaddingBottom());
+        }
     }
 
     @Override
     protected void onSizeChanged(final int w, final int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
 
-        if (cornerRadius != 0)
-            setCorerRadius(cornerRadius);
+        if (shadowSize > 0)
+            rectF.set(
+                    shadowSize,
+                    (shadowSize - shadowDy) >= 0 ? (shadowSize - shadowDy) : 0,
+                    w - shadowSize,
+                    h - (shadowSize + shadowDy));
+        else
+            rectF.set(0, 0, w, h);
+
+        float mCornerRadius = cornerRadius;
+        if (mCornerRadius < 0)
+            mCornerRadius = rectF.bottom / 2;
+
+        basePath.reset();
+        basePath.addRoundRect(rectF, mCornerRadius, mCornerRadius, Path.Direction.CW);
     }
 
     @Override
-    public void draw(Canvas canvas) {
-        super.draw(canvas);
-        SView.drawCorners(canvas, clipPath, clipPaint);
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int desiredWidth = getSuggestedMinimumWidth() + getPaddingLeft() + getPaddingRight();
+        int desiredHeight = getSuggestedMinimumHeight() + getPaddingTop() + getPaddingBottom();
+
+        setMeasuredDimension(
+                SView.measureDimension(desiredWidth, widthMeasureSpec, (int) ((shadowSize * 2))),
+                SView.measureDimension(desiredHeight, heightMeasureSpec, (int) ((shadowSize - shadowDy) + shadowSize + shadowDy)));
+    }
+
+
+    @Override
+    public void dispatchDraw(Canvas canvas) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP || shadowSize > 0) {
+            canvas.drawPath(basePath, basePaint);
+
+            if (!isInEditMode()) {
+                int saveCount = canvas.saveLayer(0, 0, getWidth(), getHeight(), null, Canvas.ALL_SAVE_FLAG);
+
+                super.dispatchDraw(canvas);
+
+                Path.FillType fillType = basePath.getFillType();
+                basePath.setFillType(Path.FillType.INVERSE_WINDING);
+                canvas.drawPath(basePath, clipPaint);
+                canvas.restoreToCount(saveCount);
+                basePath.setFillType(fillType);
+            }
+        } else
+            super.dispatchDraw(canvas);
     }
 
     /*   Corners   */
     @Override
     public void setCorerRadius(int corerRadius) {
         this.cornerRadius = corerRadius;
-        SView.setCorerRadius(this, rectF, clipPath, corerRadius);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (shadowSize > 0) {
+                setOutlineProvider(null);
+                requestLayout();
+            } else {
+                setOutlineProvider(new ViewOutlineProvider() {
+                    @Override
+                    public void getOutline(View view, Outline outline) {
+                        outline.setAlpha(0);
+                        int mCornerRadius = cornerRadius;
+                        if (mCornerRadius < 0)
+                            mCornerRadius = view.getHeight() / 2;
+                        outline.setRoundRect(0, 0, view.getWidth(), view.getHeight(), mCornerRadius);
+                    }
+                });
+            }
+        } else
+            requestLayout();
+    }
+
+    @Override
+    public void setBackgroundColor(int color) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP || shadowSize > 0) {
+            basePaint.setColor(color);
+            invalidate();
+        } else
+            super.setBackgroundColor(color);
     }
 
     @Override
