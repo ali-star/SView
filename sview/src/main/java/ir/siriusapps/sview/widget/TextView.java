67b60e6f99ee55a33b04e92e55b147a3eb66884d
ali-star <obanstar1@gmail.com>
2019-09-20 22:43:03 +0430

    update

--- sview/src/main/java/ir/siriusapps/sview/widget/TextView.java
+++ sview/src/main/java/ir/siriusapps/sview/widget/TextView.java
@@ -3,38 +3,14 @@ package ir.siriusapps.sview.widget;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Outline;
-import android.graphics.Paint;
-import android.graphics.Path;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.RectF;
 import android.graphics.Typeface;
-import android.graphics.drawable.ColorDrawable;
-import android.os.Build;
 import android.util.AttributeSet;
-import android.view.View;
-import android.view.ViewOutlineProvider;
 import androidx.annotation.Nullable;
 import ir.siriusapps.sview.R;
 import ir.siriusapps.sview.TypefaceManager;
-import ir.siriusapps.sview.view.CornerView;
 
 @SuppressLint("AppCompatCustomView")
-public class TextView extends android.widget.TextView implements CornerView {
-
-    private Path basePath = new Path();
-    private Paint clipPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private PorterDuffXfermode porterDuffXfermode = new PorterDuffXfermode(PorterDuff.Mode.CLEAR);
-    private RectF rectF = new RectF();
-    private int cornerRadius;
-
-    private Paint basePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private int shadowColor = Color.parseColor("#80000000");
-    private float shadowSize;
-    private float shadowDy;
+public class TextView extends android.widget.TextView {
 
     private String typefacePath;
 
@@ -57,151 +33,12 @@ public class TextView extends android.widget.TextView implements CornerView {
     private void init(AttributeSet attrs) {
         if (attrs != null) {
             TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.SView);
-            cornerRadius = typedArray.getDimensionPixelSize(R.styleable.SView_sview_cornerRadius, cornerRadius);
-            shadowColor = typedArray.getColor(R.styleable.SView_sview_shadowColor, shadowColor);
-            shadowSize = typedArray.getDimensionPixelSize(R.styleable.SView_sview_shadowSize, (int) shadowSize);
-            shadowDy = typedArray.getDimensionPixelSize(R.styleable.SView_sview_shadowDy, (int) shadowDy);
             typefacePath = typedArray.getString(R.styleable.SView_sview_typeface);
             typedArray.recycle();
         }
 
         if (typefacePath != null)
             setTypeface(typefacePath);
-
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP || shadowSize > 0) {
-            setLayerType(LAYER_TYPE_SOFTWARE, null);
-            setWillNotDraw(false);
-        }
-
-        clipPaint.setXfermode(porterDuffXfermode);
-
-        if (getBackground() instanceof ColorDrawable) {
-            basePaint.setColor(((ColorDrawable) getBackground()).getColor());
-        }
-
-        setBackground(null);
-
-        basePaint.setShadowLayer(shadowSize, 0, shadowDy, shadowColor);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            setClipToOutline(true);
-        }
-
-        if (shadowSize > 0)
-            setPadding((int) shadowSize + getPaddingLeft(),
-                    (int) (shadowSize - shadowDy) + getPaddingTop(),
-                    (int) shadowSize + getPaddingRight(),
-                    (int) (shadowSize + shadowDy) + getPaddingBottom());
-    }
-
-    @Override
-    protected void onSizeChanged(final int w, final int h, int oldw, int oldh) {
-        super.onSizeChanged(w, h, oldw, oldh);
-
-        if (shadowSize > 0)
-            rectF.set(
-                    shadowSize,
-                    (shadowSize - shadowDy) >= 0 ? (shadowSize - shadowDy) : 0,
-                    w - shadowSize,
-                    h - shadowSize - shadowDy);
-        else if (cornerRadius != 0) {
-            rectF.set(0, 0, w, h);
-
-            float mCornerRadius = cornerRadius;
-            if (mCornerRadius < 0)
-                mCornerRadius = rectF.bottom / 2;
-
-            basePath.reset();
-            basePath.addRoundRect(rectF, mCornerRadius, mCornerRadius, Path.Direction.CW);
-        }
-    }
-
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        if (shadowSize > 0) {
-            int desiredWidth = getSuggestedMinimumWidth() + getPaddingLeft() + getPaddingRight();
-            int desiredHeight = getSuggestedMinimumHeight() + getPaddingTop() + getPaddingBottom();
-
-            setMeasuredDimension(
-                    measureDimension(desiredWidth, widthMeasureSpec, (int) (shadowSize * 2)),
-                    measureDimension(desiredHeight, heightMeasureSpec, (int) ((shadowSize * 2) + shadowDy)));
-        } else {
-            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        }
-    }
-
-    private int measureDimension(int desiredSize, int measureSpec, int addedValue) {
-        int result;
-        int specMode = MeasureSpec.getMode(measureSpec);
-        int specSize = MeasureSpec.getSize(measureSpec);
-
-        if (specMode == MeasureSpec.EXACTLY) {
-            result = specSize;
-        } else {
-            result = desiredSize;
-            if (specMode == MeasureSpec.AT_MOST) {
-                result = Math.min(result, specSize);
-            }
-        }
-
-        return result + addedValue;
-    }
-
-
-    @Override
-    public void draw(Canvas canvas) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP || shadowSize > 0) {
-            canvas.drawPath(basePath, basePaint);
-        }
-
-        if (!isInEditMode()) {
-            int saveCount = canvas.saveLayer(0, 0, getWidth(), getHeight(), null, Canvas.ALL_SAVE_FLAG);
-
-            super.draw(canvas);
-
-            Path.FillType fillType = basePath.getFillType();
-            basePath.setFillType(Path.FillType.INVERSE_WINDING);
-            canvas.drawPath(basePath, clipPaint);
-            canvas.restoreToCount(saveCount);
-            basePath.setFillType(fillType);
-        }
-    }
-
-    /*   Corners   */
-    @Override
-    public void setCorerRadius(int corerRadius) {
-        this.cornerRadius = corerRadius;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            if (shadowSize > 0) {
-                setOutlineProvider(null);
-                requestLayout();
-            } else
-                setOutlineProvider(new ViewOutlineProvider() {
-                    @Override
-                    public void getOutline(View view, Outline outline) {
-                        outline.setAlpha(0);
-                        int mCornerRadius = cornerRadius;
-                        if (mCornerRadius < 0)
-                            mCornerRadius = view.getHeight() / 2;
-                        outline.setRoundRect(0, 0, view.getWidth(), view.getHeight(), mCornerRadius);
-                    }
-                });
-        } else
-            requestLayout();
-    }
-
-    @Override
-    public void setBackgroundColor(int color) {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP || shadowSize > 0) {
-            basePaint.setColor(color);
-            invalidate();
-        } else
-            super.setBackgroundColor(color);
-    }
-
-    @Override
-    public int getCornerRadius() {
-        return cornerRadius;
     }
 
     /*   Typeface   */
